3380ef901726a9493d205a8d03c1e992c078241d
Waldemar Naposo <waldemarnaposo313@gmail.com>
2024-09-05 23:13:11 +0700

    Implementasi Routing

---
+++ core/Application.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+// @Package app\core
+namespace app\core;
+
+class Application {
+
+    public Router $router;
+    public Request $request;
+
+    public function __construct() {
+
+        $this->request = new Request();
+        $this->router = new Router( $this->request);
+
+    }
+
+    public function run() 
+
+    {
+
+        
+
+
+        $this->router->resolve();
+    }
+
+
+
+
+}
